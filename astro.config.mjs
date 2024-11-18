0d8e4f1b4cc1e641cda5c43defb788726396ab71
harshau007 <amanupadhyay2004@gmail.com>
2024-11-18 11:25:53 +0530

    Docusaurus --> Starlight

---
+++ astro.config.mjs
@@ -0,0 +1,34 @@
+// @ts-check
+import starlight from "@astrojs/starlight";
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://arch-linux-gui.github.io",
+  base: "docs",
+  integrations: [
+    starlight({
+      title: "Arka Linux GUI",
+      logo: {
+        src: "./public/alg-logo.png",
+      },
+      social: {
+        github: "https://github.com/arch-linux-gui",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+      customCss: ["./src/styles/custom.css"],
+    }),
+  ],
+});
